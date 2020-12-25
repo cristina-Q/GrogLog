4f917a236ad8a5948c6f51cb320cd9aa4af78f9d
cristina-q <cristinamihaela@yandex.com>
2020-12-25 06:00:42 +0200

    update post head

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,5 +1,8 @@
 import { HeadPost } from './HeadPost';
 
+//receives the post to show and its meta object
+//render a single article
+
 export default function BlogPost({ children, meta }) {
   return (
     <>
