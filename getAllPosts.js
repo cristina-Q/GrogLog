a7ad06ebcd4667d330474261231b47503d05e528
cristina-q <cristinamihaela@yandex.com>
2020-12-31 05:14:21 +0200

    switch from mdx to jsx on posts

--- getAllPosts.js
+++ getAllPosts.js
@@ -1,8 +1,8 @@
 function importAll(post) {
   return post.keys().map((fileName) => ({
-    link: fileName.substr(1).replace(/\/index\.mdx$/, ''),
+    link: fileName.substr(1).replace(/\/index\.jsx$/, ''),
     module: post(fileName),
   }));
 }
 
-export const posts = importAll(require.context('./pages/blog', true, /\.mdx$/));
+export const posts = importAll(require.context('./pages/blog', true, /\.jsx$/));
